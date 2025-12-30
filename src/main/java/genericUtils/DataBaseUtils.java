92f3dbb51c30cbf41506cc52cd9e74e1c890fad9
arthi <arthi@MSI>
2025-12-30 15:07:34 +0530

    com db

--- src/main/java/genericUtils/DataBaseUtils.java
+++ src/main/java/genericUtils/DataBaseUtils.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.mysql.jdbc.Driver;
+import com.mysql.cj.jdbc.Driver;
 
 public class DataBaseUtils {
 
