54f2ebbafc8751e9d46d4a202d558e0097cbb555
arthi <arthi@MSI>
2025-12-31 12:50:53 +0530

    v

--- src/test/java/organization_module/OrganizationTest.java
+++ src/test/java/organization_module/OrganizationTest.java
@@ -6,7 +6,7 @@ import genericUtils.BaseClass;
 
 public class OrganizationTest extends BaseClass{
 	
-	@Test(retryAnalyzer = genericUtils.RetryImplementationClass.class)
+	@Test(retryAnalyzer = genericUtils.RetryImplementationClass.class, groups = "smoke")
 	public void orgTest() {
 		System.out.println("---org test----");
 	}
