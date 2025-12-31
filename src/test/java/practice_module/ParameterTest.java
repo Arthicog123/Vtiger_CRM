b56c8bb77df5f2dd897e1b0459778098814267e6
arthi <arthi@MSI>
2025-12-31 10:54:35 +0530

    para

---
+++ src/test/java/practice_module/ParameterTest.java
@@ -0,0 +1,73 @@
+package practice_module;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import objectRepo.HomePage;
+
+public class ParameterTest {
+	
+	@Test
+	public void cmdLine() throws Exception {
+		String BROWSER = System.getProperty("browser");
+		String URL = System.getProperty("url");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(BROWSER);
+		System.out.println(URL);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		
+		WebDriver driver = null;
+		// launch the browser
+		if (BROWSER.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}
+		else if (BROWSER.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		else if (BROWSER.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
+		
+		// maximize the window
+		driver.manage().window().maximize();
+		
+		// enter the url
+		driver.get(URL);
+		
+		// wait for page load
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		System.out.println("---browser launched successfully---");
+		
+		Thread.sleep(3000);
+		
+		// enter valid username and password and click on login button
+		driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+		driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+		driver.findElement(By.id("submitButton")).click();
+		
+		System.out.println("---logged into application---");
+		
+		Thread.sleep(3000);
+		
+	
+		HomePage hp = new HomePage(driver);
+		hp.signOut(driver);
+		System.out.println("---logged out from application---");
+		
+		Thread.sleep(3000);
+		
+		driver.quit();
+		System.out.println("---browser closed successfully---");
+	}
+
+}
